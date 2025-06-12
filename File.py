28954148c29d190fedf43e8150848a68417628b3
nekolay52 <shishaev090@gmail.com>
2025-06-12 16:38:52 +0400

    Initial commit

---
+++ File.py
@@ -0,0 +1,49 @@
+import os
+
+
+class File:
+    """
+    Описание но мне лень его писать
+    """
+    def __init__(self, file_name: str, create_or_not: bool, writing_content: str):
+        self.file_name = file_name
+        self.create_or_not = create_or_not
+        self.writing_content = writing_content
+        if create_or_not == True:
+            self.create()
+            if writing_content != "":
+                self.write(text = writing_content)
+                
+    def create(self):
+        with open(self.file_name, 'w') as my_file:
+            my_file.write("")
+
+    def delete(self):
+        os.remove(self.file_name)
+
+    def rename(self, new_name: str):
+        content = self.read()
+        self.delete()
+        self.file_name = new_name
+        self.write(content)
+
+    def write(self, text: str):
+        with open(self.file_name, 'w') as my_file:
+            my_file.write(text)
+
+    def read(self):
+        with open(self.file_name, 'r') as my_file:
+            content = my_file.read()
+            return content
+
+    def copy(self, new_name: str):
+        content = self.read()
+        finally_copy = File(file_name = new_name, create_or_not = True, writing_content = content)
+        return finally_copy
+
+    def append(self, text: str):
+        with open(self.file_name, 'r') as my_file:
+            content = my_file.read()
+        with open(self.file_name, 'w') as my_file:
+            my_file.write(content + text)
+
